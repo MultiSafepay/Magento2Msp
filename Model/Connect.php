5ecfd23c8f50732e3c631df918dcf4de5107b801
Ruud Jonk <ruud@multisafepay.com>
2017-01-20 12:47:18 +0100

    Set version to 1.3.0

--- Model/Connect.php
+++ Model/Connect.php
@@ -358,7 +358,7 @@ class Connect extends \Magento\Payment\Model\Method\AbstractMethod
             "plugin" => array(
                 "shop" => $magentoInfo->getName() . ' ' . $magentoInfo->getVersion() . ' ' . $magentoInfo->getEdition(),
                 "shop_version" => $magentoInfo->getVersion(),
-                "plugin_version" => ' - Plugin 1.2.0',
+                "plugin_version" => ' - Plugin 1.3.0',
                 "partner" => "MultiSafepay",
             ),
             "gateway_info" => array(
