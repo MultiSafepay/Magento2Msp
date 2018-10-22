1b29feabaef8e1cdd9290215d39332db55ae4a2a
Jasper Nadi <jasper.nadi@multisafepay.com>
2018-10-22 15:38:02 +0200

    PLGMAGTWOS-329: Bump version and changelog

--- Model/Connect.php
+++ Model/Connect.php
@@ -580,7 +580,7 @@ class Connect extends \Magento\Payment\Model\Method\AbstractMethod
                 "plugin" => array(
                     "shop" => $magentoInfo->getName() . ' ' . $magentoInfo->getVersion() . ' ' . $magentoInfo->getEdition(),
                     "shop_version" => $magentoInfo->getVersion(),
-                    "plugin_version" => ' - Plugin 1.6.1',
+                    "plugin_version" => ' - Plugin 1.6.2',
                     "partner" => "MultiSafepay",
                 ),
                 "gateway_info" => array(
