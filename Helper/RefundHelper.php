9ce3bb59508670fb40e6cd227bb4d5db4ad6665c
Vinod MultiSafepay <vinod.sowdagar@multisafepay.com>
2019-09-25 16:27:05 +0200

    PLGMAGTWOS-476: [chore] Change @author email address in disclaimer

--- Helper/RefundHelper.php
+++ Helper/RefundHelper.php
@@ -16,7 +16,7 @@
  *
  * @category    MultiSafepay
  * @package     Connect
- * @author      MultiSafepay <techsupport@multisafepay.com>
+ * @author      MultiSafepay <integration@multisafepay.com>
  * @copyright   Copyright (c) 2019 MultiSafepay, Inc. (https://www.multisafepay.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *
