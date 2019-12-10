39298982172297108a6ca301fad0192670067305
Vinod Sowdagar <53818758+Vinod-MultiSafepay@users.noreply.github.com>
2019-12-10 16:03:37 +0100

    [ci] Fix failing integration tests for isAvailable function

--- Test/Integration/Model/Gateways/IdealTest.php
+++ Test/Integration/Model/Gateways/IdealTest.php
@@ -2,11 +2,8 @@
 
 namespace MultiSafepay\Connect\Test\Integration\Model;
 
-use Magento\Customer\Api\AccountManagementInterface;
 use Magento\Customer\Api\AddressRepositoryInterface;
-use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Address;
 use Magento\TestFramework\ObjectManager;
@@ -31,42 +28,38 @@ class IdealTest extends TestCase
 
     /**
      * @return Quote
-     * @throws LocalizedException
-     * @throws NoSuchEntityException
      */
-    protected function createQuote()
+    protected function createQuote(): Quote
+    {
+        /** @var Quote $quote */
+        $quote = $this->objectManager->create(Quote::class);
+        $quote->setStoreId(1)
+            ->setIsActive(true)
+            ->setIsMultiShipping(false)
+            ->setCheckoutMethod('customer')
+            ->setReservedOrderId('test_order_1')
+            ->setCustomerEmail('aaa@aaa.com')
+            ->setQuoteCurrencyCode('EUR')
+            ->setBaseGrandTotal(50);
+
+        return $quote;
+    }
+
+    /**
+     * @return Address
+     * @throws LocalizedException
+     */
+    protected function createShippingAddress(): Address
     {
         /** @var Address $quoteShippingAddress */
         $quoteShippingAddress = $this->objectManager->create(Address::class);
-
-        /** @var AccountManagementInterface $accountManagement */
-        $accountManagement = $this->objectManager->create(AccountManagementInterface::class);
-
-        /** @var CustomerRepositoryInterface $customerRepository */
-        $customerRepository = $this->objectManager->create(CustomerRepositoryInterface::class);
-        $customer = $customerRepository->getById(1);
 
         /** @var AddressRepositoryInterface $addressRepository */
         $addressRepository = $this->objectManager->create(AddressRepositoryInterface::class);
         $quoteShippingAddress->importCustomerAddressData($addressRepository->getById(1));
         $quoteShippingAddress->setShippingMethod('flatrate_flatrate');
 
-        /** @var Quote $quote */
-        $quote = $this->objectManager->create(Quote::class);
-        $quote->setStoreId(1)
-          ->setIsActive(true)
-          ->setIsMultiShipping(false)
-          ->assignCustomerWithAddressChange($customer)
-          ->setShippingAddress($quoteShippingAddress)
-          ->setBillingAddress($quoteShippingAddress)
-          ->setCheckoutMethod('customer')
-          ->setPasswordHash($accountManagement->getPasswordHash('password'))
-          ->setReservedOrderId('test_order_1')
-          ->setCustomerEmail('aaa@aaa.com')
-          ->setQuoteCurrencyCode('EUR')
-          ->setBaseGrandTotal(50);
-
-        return $quote;
+        return $quoteShippingAddress;
     }
 
     /**
@@ -76,10 +69,12 @@ class IdealTest extends TestCase
      * @magentoConfigFixture default_store gateways/ideal/allowed_currency EUR
      * @magentoConfigFixture default_store gateways/ideal/min_order_total 20
      * @magentoConfigFixture default_store gateways/ideal/max_order_total 100
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnTrueIfOrderAmountIsWithinRestrictions()
     {
         $result = $this->idealInstance->isAvailable($this->createQuote());
+
         $this->assertTrue($result);
     }
 
@@ -88,15 +83,11 @@ class IdealTest extends TestCase
      * @magentoConfigFixture default_store gateways/ideal/allowed_currency EUR
      * @magentoConfigFixture default_store gateways/ideal/min_order_total 20
      * @magentoConfigFixture default_store gateways/ideal/max_order_total 100
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnFalseIfOrderAmountIsNotWithinRestrictions()
     {
-        $result = $this->idealInstance->isAvailable(
-            $this->createQuote(
-            )->setBaseGrandTotal(
-                120
-            )
-        );
+        $result = $this->idealInstance->isAvailable($this->createQuote()->setBaseGrandTotal(120));
         $this->assertFalse($result);
     }
 
@@ -105,6 +96,7 @@ class IdealTest extends TestCase
      *
      * @magentoConfigFixture default_store gateways/ideal/active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_currency EUR
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnTrueIfCurrencyIsAllowed()
     {
@@ -115,6 +107,7 @@ class IdealTest extends TestCase
     /**
      * @magentoConfigFixture default_store gateways/ideal/active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_currency USD
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnFalseIfCurrencyIsNotAllowed()
     {
@@ -123,24 +116,28 @@ class IdealTest extends TestCase
     }
 
     /**
-     * Start testing allowed groups
-     *
      * @magentoConfigFixture default_store gateways/ideal/active 1
-     * @magentoConfigFixture default_store gateways/ideal/allowed_groups 1,2
+     * @magentoConfigFixture default_store gateways/ideal/allowed_groups 0,2
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnTrueIfGroupIsAllowed()
     {
         $result = $this->idealInstance->isAvailable($this->createQuote());
+
         $this->assertTrue($result);
     }
 
     /**
+     * Start testing allowed groups
+     *
      * @magentoConfigFixture default_store gateways/ideal/active 1
-     * @magentoConfigFixture default_store gateways/ideal/allowed_groups 0,2
+     * @magentoConfigFixture default_store gateways/ideal/allowed_groups 1,2
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnFalseIfGroupIsNotAllowed()
     {
         $result = $this->idealInstance->isAvailable($this->createQuote());
+
         $this->assertFalse($result);
     }
 
@@ -149,6 +146,7 @@ class IdealTest extends TestCase
      *
      * @magentoConfigFixture default_store gateways/ideal/active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_carrier_active 0
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
      */
     public function testIsAvailableShouldReturnTrueIfShippingMethodRestrictionIsDisabled()
     {
@@ -160,10 +158,14 @@ class IdealTest extends TestCase
      * @magentoConfigFixture default_store gateways/ideal/active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_carrier_active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_carrier flatrate_flatrate
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
+     * @throws LocalizedException
      */
     public function testIsAvailableShouldReturnTrueIfShippingMethodIsAllowed()
     {
-        $result = $this->idealInstance->isAvailable($this->createQuote());
+        $result = $this->idealInstance->isAvailable(
+            $this->createQuote()->setShippingAddress($this->createShippingAddress())
+        );
         $this->assertTrue($result);
     }
 
@@ -171,10 +173,14 @@ class IdealTest extends TestCase
      * @magentoConfigFixture default_store gateways/ideal/active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_carrier_active 1
      * @magentoConfigFixture default_store gateways/ideal/allowed_carrier tablerate_bestway
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_address.php
+     * @throws LocalizedException
      */
     public function testIsAvailableShouldReturnFalseIfShippingMethodIsNotAllowed()
     {
-        $result = $this->idealInstance->isAvailable($this->createQuote());
+        $result = $this->idealInstance->isAvailable(
+            $this->createQuote()->setShippingAddress($this->createShippingAddress())
+        );
         $this->assertFalse($result);
     }
 }
